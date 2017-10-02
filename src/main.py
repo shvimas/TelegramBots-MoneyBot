1e4f2b500cdd542dfbcc4808f3cbe60a6cc0a257
shvimas <shvimerd@gmail.com>
2017-10-02 21:43:42 +0300

    try to make bot immortal

--- src/main.py
+++ src/main.py
@@ -1,16 +1,8 @@
 import os
-from datetime import datetime
 
 if __name__ == "__main__":
-    print("Started bot at " + str(datetime.now()))
-    os.system("python bot_impl.py")
-    '''
     try:
-        log = open("log.txt", "a")
-        log.write("Started bot at " + str(datetime.now()) + "\n")
-        log.close()
-        os.system("python bot_impl.py >> log.txt 2>&1")
-    except IOError as e:
-        print("Failed to start logging: " + e.strerror)
         os.system("python bot_impl.py")
-    '''
+    except Exception as e:
+        print(str(e))
+        os.system("python main.py")
